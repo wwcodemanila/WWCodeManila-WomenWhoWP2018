928997ee35e90914e630278ba213fc81ff00092d
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-10-13 15:03:41 +0800

    Add new component for EventDetail and use it in EventDetails

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import EventDetail from './EventDetail';
+
 import schedule from './../assets/images/schedule.png';
 import venue from './../assets/images/venue.png';
 import attire from './../assets/images/attire.png';
@@ -14,39 +16,10 @@ class EventDetails extends Component {
           <Col md="1"></Col>
           <Col md="10">
             <Row>
-              <Col>
-                <img src={schedule} alt="Calendar with check icon" />
-                <h2>
-                  SAT Oct 27, 2018
-                  <br />
-                  12:30 to 6:00PM
-                </h2>
-              </Col>
-              <Col>
-                <img src={venue} alt="Map with pin icon" />
-                <h2>Zendesk</h2>
-                <p>
-                  30/F Netpark Building
-                  <br />
-                  Bonifacio Global City
-                </p>
-              </Col>
-              <Col>
-                <img src={attire} alt="Shirt icon" />
-                <h2>Bldg. Attire</h2>
-                <p>
-                  No Shorts, Sando, Slippers,
-                  <br />
-                  Casual Attire will do
-                </p>
-              </Col>
-              <Col>
-                <img src={rsvp} alt="Folded paper icon" />
-                <h2>Register & Reserve</h2>
-                <p>
-                  http://bit.ly/womenwhowordpress
-                </p>
-              </Col>
+              <EventDetail icon={schedule} alt="Calendar with check icon" heading="SAT Oct 27, 2018<br />12:30 to 6:00PM" paragraph="" />
+              <EventDetail icon={venue} alt="Map with pin icon" heading="Zendesk" paragraph="30/F Netpark Building<br />Bonifacio Global City" />
+              <EventDetail icon={attire} alt="Shirt icon" heading="Bldg. Attire" paragraph="No Shorts, Sando, Slippers,<br />Casual Attire will do" />
+              <EventDetail icon={rsvp} alt="Folded paper icon" heading="Register & Reserve" paragraph="http://bit.ly/womenwhowordpress" />
             </Row>
           </Col>
           <Col md="1"></Col>
