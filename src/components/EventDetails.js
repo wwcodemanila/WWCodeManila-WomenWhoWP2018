8a9a55ddca3a90ffe357d8f27b27139ea4ffff1a
Regina Alyssa <reginaalyssa01809@gmail.com>
2018-10-13 15:21:42 +0800

    Use Montserrat font and remove comments

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -56,10 +56,6 @@ class EventDetails extends Component {
           <Col md="10">
             <Row>
               {this.createEventDetails()}
-              {/*<EventDetail icon={schedule} alt="Calendar with check icon" heading="SAT Oct 27, 2018<br />12:30 to 6:00PM" paragraph="" />*/}
-              {/*<EventDetail icon={venue} alt="Map with pin icon" heading="Zendesk" paragraph="30/F Netpark Building<br />Bonifacio Global City" />*/}
-              {/*<EventDetail icon={attire} alt="Shirt icon" heading="Bldg. Attire" paragraph="No Shorts, Sando, Slippers,<br />Casual Attire will do" />*/}
-              {/*<EventDetail icon={rsvp} alt="Folded paper icon" heading="Register & Reserve" paragraph="http://bit.ly/womenwhowordpress" />*/}
             </Row>
           </Col>
           <Col md="1"></Col>
