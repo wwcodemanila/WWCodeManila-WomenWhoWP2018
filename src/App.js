aff58b28b894dc586daf727b601d1d8396beee2b
Rochelle Ilustre <chellie.ilustre@gmail.com>
2018-10-13 16:09:32 +0800

    added banner and navigation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Jumbotron, Button } from "reactstrap";
+import Navigation from "./components/Navigation.js";
+import Banner from "./components/Banner.js";
 
 const repository =
   "https://docs.google.com/presentation/d/1z3XGsAQzCW66UKmf6ObbgkddTIc2TveFgkHSAaY9lFg/edit#slide=id.g43f5d69b4a_0_8";
@@ -8,6 +10,9 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <Navigation/>
+        <Banner/>
+
         <Jumbotron>
           <h1 className="display-3">Hello, Women Who Code Manila!</h1>
           <p className="lead">
