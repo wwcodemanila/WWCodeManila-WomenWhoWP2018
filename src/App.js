ce95c3efb9048777dbfd20c41e2b0d9823fb6d7a
Mark Kevin King <king.mark.kevin@icloud.com>
2018-10-13 17:02:25 +0800

    Added Footer section

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Jumbotron, Button } from "reactstrap";
+import Footer from "./components/Footer"
 
 const repository =
   "https://docs.google.com/presentation/d/1z3XGsAQzCW66UKmf6ObbgkddTIc2TveFgkHSAaY9lFg/edit#slide=id.g43f5d69b4a_0_8";
@@ -8,23 +9,7 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Jumbotron>
-          <h1 className="display-3">Hello, Women Who Code Manila!</h1>
-          <p className="lead">
-            Together we'll create the new landing page for{" "}
-            <strong>manila.womenwhocode.com</strong>. We'll use{" "}
-            <strong>ReactJS</strong> in this activity.
-          </p>
-          <hr className="my-2" />
-          <p>Happy hacking!</p>
-          <p className="lead">
-            <Button outline color="primary">
-              <a href={repository} target="__blank">
-                Learn More
-              </a>
-            </Button>
-          </p>
-        </Jumbotron>
+        <Footer/>
       </React.Fragment>
     );
   }
