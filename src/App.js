c817f54a67f9760a59bd47c199368d9829e7a075
Kim Morano <ksmorano@gmail.com>
2018-10-13 17:14:24 +0800

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React, { Component } from "react";
-import { Jumbotron, Button } from "reactstrap";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
 import Navigation from "./components/Navigation.js";
 import Banner from "./components/Banner.js";
 import About from "./components/About";
-import EventDetails from './components/EventDetails';
+import EventDetails from "./components/EventDetails";
 
 const repository =
   "https://docs.google.com/presentation/d/1z3XGsAQzCW66UKmf6ObbgkddTIc2TveFgkHSAaY9lFg/edit#slide=id.g43f5d69b4a_0_8";
@@ -13,11 +12,11 @@ class App extends Component {
   render() {
     return (
       <React.Fragment>
-        <Footer/>
-        <Navigation/>
-        <Banner/>
+        <Navigation />
+        <Banner />
         <EventDetails />
         <About />
+        <Footer />
       </React.Fragment>
     );
   }
